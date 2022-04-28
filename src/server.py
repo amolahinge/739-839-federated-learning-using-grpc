3c4bb79768852373a827b2dc8c449a8d88aff432
Kalpit Munot <f2014259@alumni.bits-pilani.ac.in>
2022-04-27 19:34:27 -0600

    Added replication to backup

--- src/server.py
+++ src/server.py
@@ -1,7 +1,10 @@
 from __future__ import print_function
+from inspect import getmembers
 
 import logging
 
+from concurrent import futures
+import argparse
 import grpc
 import federated_pb2
 import federated_pb2_grpc
@@ -15,35 +18,40 @@ import numpy as np
 import threading
 import torchvision
 import torchvision.transforms as transforms
+from pathlib import Path
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 from models import *
 
 clients=[]
 optimModelPath = "optimizedModel.pth"
+backupServerChannel = None
+
+isPrimary = True
+options = [
+    ('grpc.max_send_message_length', 1024 * 1024 * 1024),
+    ('grpc.max_receive_message_length', 1024 * 1024 * 1024)
+]
+
+def getMountedPath(suffixPath):
+    return mountPoint + '/'+ suffixPath
 
 def trainThreadFunc(count,channel):
     net = MobileNet()
     stub = federated_pb2_grpc.TrainerStub(channel)
     response = stub.StartTrain(federated_pb2.TrainRequest(name='you',rank=count,world=len(clients)))
     model=base64.b64decode(response.message)
-    f = open("test_"+str(count)+".pth",'wb')
+    f = open(getMountedPath("test_"+str(count)+".pth"),'wb')
     f.write(model)
     f.close()
 
-def sendThreadFunc(count,channel):
-    
-    f=open(optimModelPath, "rb")
+def sendOptimizedModel(channel):
+    f=open(getMountedPath(optimModelPath), "rb")
     encode=base64.b64encode(f.read())
     f.close()
     stub = federated_pb2_grpc.TrainerStub(channel)
     response = stub.SendModel(federated_pb2.SendModelRequest(model=encode))
 
 def run():
-    options = [
-        ('grpc.max_send_message_length', 1024 * 1024 * 1024),
-        ('grpc.max_receive_message_length', 1024 * 1024 * 1024)
-    ]
-
     channels=[]
     count=0
     # threads=[]
@@ -62,18 +70,26 @@ def run():
         allreduce()
         
         sendThreads=[]
+        if backupServerChannel is not None:
+            sendOptimizedModel(backupServerChannel)
         for count,channel in enumerate(channels):
-            sendThreads.append(threading.Thread(target=sendThreadFunc, args=(count,channel)))
+            sendThreads.append(threading.Thread(target=sendOptimizedModel, args=(channel,)))
         for i in range(len(sendThreads)):
             sendThreads[i].start()
         for i in range (len(sendThreads)):
             sendThreads[i].join()
 
+def ConnectToBackupServer(altaddress, port):
+    addr = altaddress + ":" + port
+    global backupServerChannel
+    backupServerChannel = grpc.insecure_channel(addr, options=options)
+
 def allreduce():
     pushedModels = []
     for i in range(0, len(clients)):
         m = MobileNet()
-        m.load_state_dict(torch.load("test_"+str(i)+".pth")['net'])
+        path = getMountedPath("test_"+str(i)+".pth")
+        m.load_state_dict(torch.load(path)['net'])
         pushedModels.append(m)
 
     # print("Before summing", pushedModels[0].state_dict())
@@ -95,12 +111,51 @@ def allreduce():
         'acc': 1,
         'epoch': 1,
     }
-    torch.save(state, optimModelPath)
+    torch.save(state, getMountedPath(optimModelPath))
 
+#######################################################################
+# Functions related to Backup server only
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10),options = [
+        ('grpc.max_send_message_length', 1024 * 1024 * 1024),
+        ('grpc.max_receive_message_length', 1024 * 1024 * 1024)
+    ])
+    federated_pb2_grpc.add_TrainerServicer_to_server(Trainer(), server)
+    server.add_insecure_port('[::]:8080')
+    server.start()
+    server.wait_for_termination()
+
+class Trainer(federated_pb2_grpc.TrainerServicer):
+    def SendModel(self,request,context):
+        print("This function started running")
+        f = open(getMountedPath(optimModelPath), "wb")
+        decode = base64.b64decode(request.model)
+        f.write(decode)
+        f.close()
+        return federated_pb2.SendModelReply(reply = "success")
+
+#######################################################################
 
 if __name__ == '__main__':
     logging.basicConfig()
     clients.append('localhost:50051')
     #clients.append('localhost:50052')
 
-    run()
+    parser = argparse.ArgumentParser(description='Server Information')
+    parser.add_argument('--p', default='n', help='Is Primary?')
+    parser.add_argument('--altAddress', default='', help='Backup Server address')
+    parser.add_argument('--port', default='', help='Port')
+    args = parser.parse_args()
+
+    if args.p == 'y':
+        print("Primary triggered")
+        ConnectToBackupServer(args.altAddress, args.port)
+        mountPoint = "Primary"
+        Path(mountPoint).mkdir(parents=True, exist_ok=True)
+        run()
+    else:
+        print("Backup triggered")
+        mountPoint = "Backup"
+        Path(mountPoint).mkdir(parents=True, exist_ok=True)
+        serve()
+
