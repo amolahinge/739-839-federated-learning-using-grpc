d90f02858f9ee0e1eef5b039ebaa9cc52aa54e9a
amolahinge <kalpit12@pollingnode.kalpit12-124029.greyfiber-pg0.utah.cloudlab.us>
2022-04-28 02:31:01 -0600

    client failure

--- src/federated_pb2_grpc.py
+++ src/federated_pb2_grpc.py
@@ -24,6 +24,11 @@ class TrainerStub(object):
                 request_serializer=federated__pb2.SendModelRequest.SerializeToString,
                 response_deserializer=federated__pb2.SendModelReply.FromString,
                 )
+        self.HeartBeat = channel.unary_unary(
+                '/federated.Trainer/HeartBeat',
+                request_serializer=federated__pb2.Request.SerializeToString,
+                response_deserializer=federated__pb2.HeartBeatResponse.FromString,
+                )
 
 
 class TrainerServicer(object):
@@ -42,6 +47,12 @@ class TrainerServicer(object):
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
+    def HeartBeat(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
 
 def add_TrainerServicer_to_server(servicer, server):
     rpc_method_handlers = {
@@ -54,6 +65,11 @@ def add_TrainerServicer_to_server(servicer, server):
                     servicer.SendModel,
                     request_deserializer=federated__pb2.SendModelRequest.FromString,
                     response_serializer=federated__pb2.SendModelReply.SerializeToString,
+            ),
+            'HeartBeat': grpc.unary_unary_rpc_method_handler(
+                    servicer.HeartBeat,
+                    request_deserializer=federated__pb2.Request.FromString,
+                    response_serializer=federated__pb2.HeartBeatResponse.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
@@ -96,5 +112,22 @@ class Trainer(object):
         return grpc.experimental.unary_unary(request, target, '/federated.Trainer/SendModel',
             federated__pb2.SendModelRequest.SerializeToString,
             federated__pb2.SendModelReply.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def HeartBeat(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/federated.Trainer/HeartBeat',
+            federated__pb2.Request.SerializeToString,
+            federated__pb2.HeartBeatResponse.FromString,
             options, channel_credentials,
             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
--- src/server.py
+++ src/server.py
@@ -2,6 +2,8 @@ from __future__ import print_function
 
 import logging
 import argparse
+import time
+import copy
 
 import grpc
 import federated_pb2
@@ -19,46 +21,96 @@ import torchvision.transforms as transforms
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 from models import *
 
-clients=[]
+
+channels={} #client - client channel
+clients={} # client - active / inactive status
 compressFlag = False
 optimModelPath = "optimizedModel.pth"
 
-def trainThreadFunc(count,channel):
-    net = MobileNet()
-    stub = federated_pb2_grpc.TrainerStub(channel)
-    response = stub.StartTrain(federated_pb2.TrainRequest(name='you',rank=count,world=len(clients)))
-    model=base64.b64decode(response.message)
-    f = open("test_"+str(count)+".pth",'wb')
-    f.write(model)
-    f.close()
+options = [
+    ('grpc.max_send_message_length', 1024 * 1024 * 1024),
+    ('grpc.max_receive_message_length', 1024 * 1024 * 1024)
+    ]
 
-def sendThreadFunc(count,channel):
+def trainThreadFunc(count, client, channel):
+    stub = federated_pb2_grpc.TrainerStub(channel)
+    try:
+        response = stub.StartTrain(federated_pb2.TrainRequest(rank=count,world=len(clients)))
+        model=base64.b64decode(response.message)
+        f = open("test_"+str(count)+".pth",'wb')
+        f.write(model)
+        f.close()
+    except grpc.RpcError as e:
+        status_code = e.code()
+        if grpc.StatusCode.UNAVAILABLE == status_code:
+            clients[client] = False
+
+def sendThreadFunc(count,client, channel):
     
     f=open(optimModelPath, "rb")
     encode=base64.b64encode(f.read())
     f.close()
-    stub = federated_pb2_grpc.TrainerStub(channel)
-    response = stub.SendModel(federated_pb2.SendModelRequest(model=encode))
+    try:
+        stub = federated_pb2_grpc.TrainerStub(channel)
+        response = stub.SendModel(federated_pb2.SendModelRequest(model=encode))
+    except grpc.RpcError as e:
+        status_code = e.code()
+        if grpc.StatusCode.UNAVAILABLE == status_code:
+            clients[client] = False            
+            #del channels[client] 
+
+def checkClientStatus():
+    #logic to check if we are able to re-establish connection with client
+    while(True):
+        time.sleep(1)
+        clients_copy = copy.copy(clients)
+        for key in clients_copy.keys():
+            #iterate over inactive clients only
+            if not clients_copy[key]:
+                try:
+                    #creating new channel here
+                    channel = createChannel(key)
+                    stub = federated_pb2_grpc.TrainerStub(channel)
+                    response = stub.HeartBeat(federated_pb2.Request())
+                    #if there is no error here
+                    if response.status == 1:
+                        clients[key] = True
+                        channels[key] = channel
+                        #send updated model again
+                        print("Sending updated model with count value", list(clients_copy.keys()).index(key))
+                        sendThreadFunc(list(clients_copy.keys()).index(key),key,channel)
+                except grpc.RpcError as e:
+                    status_code = e.code()
+                    #print("GRPC Error", status_code)
+        print("Client status", clients)
+
+def createChannel(client):
+    if compressFlag:
+        return grpc.insecure_channel(client,options=options,compression=grpc.Compression.Gzip)
+    else:
+        return grpc.insecure_channel(client,options=options)
+
+def init():
+    for client in clients.keys():
+        channels[client] = createChannel(client)
 
 def run():
-    options = [
-        ('grpc.max_send_message_length', 1024 * 1024 * 1024),
-        ('grpc.max_receive_message_length', 1024 * 1024 * 1024)
-    ]
+    init()
+    #fault tolerance
+    clientTracking = threading.Thread(target=checkClientStatus)
+    clientTracking.start()
 
-    channels=[]
-    count=0
-    # threads=[]
-    for client in clients:
-        if compressFlag:
-            channels.append(grpc.insecure_channel(client,options=options,compression=grpc.Compression.Gzip))
-        else:
-            channels.append(grpc.insecure_channel(client,options=options))
-    for epoch in range(2):
-        net = MobileNet()
+    for epoch in range(20):
+        print("Starting epoch", epoch)
         trainthreads=[]
-        for count,channel in enumerate(channels):
-            trainthreads.append(threading.Thread(target=trainThreadFunc, args=(count,channel)))
+        #print(len(channels))
+        count = 0
+        for client,channel in channels.items():
+            if clients[client]:
+                trainthreads.append(threading.Thread(target=trainThreadFunc, args=(count,client,channel)))
+                count = count + 1
+        print("Train thread count", count)
+
         for i in range(len(trainthreads)):
             trainthreads[i].start()
         for i in range (len(trainthreads)):
@@ -66,8 +118,14 @@ def run():
         allreduce()
         
         sendThreads=[]
-        for count,channel in enumerate(channels):
-            sendThreads.append(threading.Thread(target=sendThreadFunc, args=(count,channel)))
+        count = 0
+
+        for client,channel in channels.items():
+            if clients[client]:
+                sendThreads.append(threading.Thread(target=sendThreadFunc, args=(count,client,channel)))
+                count = count + 1
+        print("Send updated model thread count", count)
+
         for i in range(len(sendThreads)):
             sendThreads[i].start()
         for i in range (len(sendThreads)):
@@ -80,19 +138,16 @@ def allreduce():
         m.load_state_dict(torch.load("test_"+str(i)+".pth")['net'])
         pushedModels.append(m)
 
-    # print("Before summing", pushedModels[0].state_dict())
     optimizedModel = pushedModels[0].state_dict()
     
     for index in range(1, len(pushedModels)):
         m = pushedModels[index].state_dict()
         for key in m:
             optimizedModel[key] = optimizedModel[key] + m[key]
-    # print("After summing", optimizedModel)
 
     for key in optimizedModel:
         optimizedModel[key] = optimizedModel[key] / len(pushedModels)
 
-    # print("After averaging", optimizedModel)
 
     state = {
         'net': optimizedModel,
@@ -109,11 +164,11 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     if args.compressFlag == "Y": 
-        compressFlag = True
-        
+        compressFlag = True        
     print("Compression {} enabled".format(args.compressFlag))
-
-    clients.append('localhost:50051')
-    clients.append('localhost:50052')
-
+    
+    clients['localhost:50051'] = True
+    clients['localhost:50052'] = True
+    #clients.append('localhost:50053')
+    #clients.append('localhost:50054')
     run()
